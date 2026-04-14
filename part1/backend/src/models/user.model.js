5eea29d5f68415b884642e090ae73caf2232313e
Gifty713 <giftj5584@gmail.com>
2026-04-14 23:17:48 +0100

    recent commit

--- part1/backend/src/models/user.model.js
+++ part1/backend/src/models/user.model.js
@@ -31,12 +31,11 @@ const userSchema = new Schema(
     }
 );
 // hash password for security
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function(){
     if(!this.isModified("password")){
-        return next();
+        return;
     }
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 }); 
 // compare passwords
 userSchema.methods.comparePassword = async function (password) {
