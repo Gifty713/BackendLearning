9406fb35335054f0438342ef1c8dda2beec09fbd
Gifty713 <giftj5584@gmail.com>
2026-04-14 19:38:47 +0100

    recent commit

--- part1/backend/src/app.js
+++ part1/backend/src/app.js
@@ -3,8 +3,10 @@ import router from "./routes/auth.route";
 // create express app
 const app = express(); 
 app.use(express.json())
+
 // declaring routes
 app.use("/api/v1/users", router);
 
 // example route: http://localhost:4000/api/v1/users/register
-export default app;
+
+export default app; 
--- part1/backend/src/routes/auth.route.js
+++ part1/backend/src/routes/auth.route.js
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { registerUser } from "../controllers/auth.controllers";
 const router = Router();
-export default router;
+
+router.post('/register').post(registerUser)
+export default router; 
