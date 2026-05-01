e98b714f70a65c6693eb8c86162e750802c65b13
Gifty713 <giftj5584@gmail.com>
2026-05-01 07:09:01 +0100

    last commit

--- part1/backend/src/config/database.js
+++ part1/backend/src/config/database.js
@@ -2,8 +2,7 @@
 import mongoose from "mongoose";
 const connectDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect
-        (`${process.env.MONGODB_URI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(`\nConnection successful!!
             ${connectionInstance.connection.host}`)
     } catch (error) {
